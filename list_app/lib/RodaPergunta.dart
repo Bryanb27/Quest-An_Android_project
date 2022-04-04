03b4defbd9971bf24df4768df37b2c2b9deb5934
Bryan Jonathan <bryanb27hadouken@gmail.com>
2022-04-04 20:21:03 -0300

    Adicionado transição de telas

--- list_app/lib/RodaPergunta.dart
+++ list_app/lib/RodaPergunta.dart
@@ -1,8 +1,14 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'dart:io';
 
-class RodaPergunta extends StatelessWidget {
+class RodaPergunta extends StatefulWidget{
+  @override
+  _RodaPergunta createState() => _RodaPergunta();
+}
+
+class _RodaPergunta extends State<RodaPergunta> {
   StreamController<int> selected = StreamController<int>();
 
  /* @override
@@ -19,31 +25,42 @@ class RodaPergunta extends StatelessWidget {
       'Ciências',
       'História',
     ];
+    String _tipoPergunta;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Fortune Wheel'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          // setState(() {
-          selected.add(
-            Fortune.randomInt(0, items.length),
-          );
-        },
-        child: Column(
-          children: [
-            Expanded(
-              child: FortuneWheel(
-                selected: selected.stream,
-                items: [
-                  for (var it in items) FortuneItem(child: Text(it)),
-                ],
+      body: Container(
+        color: Colors.amber,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+            selected.add(
+              Fortune.randomInt(0, items.length),
+            );
+            _tipoPergunta = selected.toString();
+            });
+            //sleep(Duration(seconds:1));
+
+          },
+
+          child: Column(
+            children: [
+              Expanded(
+                child: FortuneWheel(
+                  selected: selected.stream,
+                  items: [
+                    for (var it in items) FortuneItem(child: Text(it)),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+
     );
+
   }
 }
--- list_app/lib/main.dart
+++ list_app/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'Home.dart';
 import 'RodaPergunta.dart';
+import 'Pergunta.dart';
+
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: "/",
     routes: {
       "/Roda_da_pergunta": (context) => RodaPergunta(),
+      "/Pergunta": (context) => Pergunta(),
     },
     home: Home(),
   ));
